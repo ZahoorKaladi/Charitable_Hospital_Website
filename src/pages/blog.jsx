3d900cb29ed3b636d1e08f83543d70f3cb4d06ae
Zahoor Ahmed <82461526+ZahoorKaladi@users.noreply.github.com>
2025-09-16 14:32:01 +0500

    Update blog.jsx

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -81,7 +81,7 @@ const Blog = () => {
               >
                 <Link to={`/blog/${post.slug}`}>
                   <div className="relative w-full h-48 overflow-hidden">
-                    <img src={`${import.meta.env.VITE_STRAPI_URL}${post.imageUrl}`}
+                    <img src={post.imageUrl}
                       alt={post.title}
                       className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
                     />
