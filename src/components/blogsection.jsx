8307e9d7051cf6c6b85bdba95b44bf6bec18264e
Zahoor Ahmed <82461526+ZahoorKaladi@users.noreply.github.com>
2025-09-16 12:55:08 +0500

    Update blogsection.jsx

--- src/components/blogsection.jsx
+++ src/components/blogsection.jsx
@@ -95,7 +95,8 @@ const BlogSection = () => {
                 >
                   <div className="relative w-full h-48 overflow-hidden">
                     <img
-                      src={post.imageUrl}
+                      src={post.imageUrl} //it use images from Cloudinary
+                     // src={`${import.meta.env.VITE_STRAPI_URL}${post.imageUrl}`} 
                       alt={post.title}
                       className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
                     />
