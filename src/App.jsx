273ff11cdbf1acff9b3bfe2693111c194c942a9e
Zahoor Ahmed <82461526+ZahoorKaladi@users.noreply.github.com>
2025-09-16 14:59:13 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,38 @@
 // src/App.jsx
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Service from "./pages/service";
 import Donation from "./pages/donation";
+//import BlogPage from "./pages/blog"; // <-- Your main blog page
+import BlogPostPage from "./pages/blog/[slug]"; // <-- Import the new dynamic page
+//import Blog from "./pages/Blog";
 import Blog from "./pages/blog";
-import BlogPostPage from "./pages/blog/[slug]";
-import ServiceDetailPage from "./pages/service/[slug]";
+import ServiceDetailPage from "./pages/services/[slug]";
 import Volunteer from "./pages/volunteer";
-import Layout from "./components/Layout";
-import ScrollToTop from "./components/scrolltotop";
 
 function App() {
   return (
-    <BrowserRouter>
-      {/* This is the main fix. overflow-x-hidden prevents the horizontal scrollbar 
-        that was causing the page to be dragged left and right on mobile. 
+    <div>
+      {/*
+        The Navbar and Footer are now in Layout.jsx.
+        You only define your routes here.
       */}
-      <div className="overflow-x-hidden">
-        <ScrollToTop />
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/service" element={<Service />} />
-            <Route path="/service/:slug" element={<ServiceDetailPage />} />
-            <Route path="/donation" element={<Donation />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/:slug" element={<BlogPostPage />} />
-            <Route path="/volunteer" element={<Volunteer />} />
-          </Routes>
-        </Layout>
-      </div>
-    </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/donation" element={<Donation />} />
+        <Route path="/blog" element={<Blog />} /> {/* <-- Route for the list of all post*/}
+        <Route path="/blog/:slug" element={<BlogPostPage />} /> 
+       <Route path="/volunteer" element={<Volunteer />} />
+        
+        {/* <-- New dynamic route for a single post */}
+        <Route path="/service/:slug" element={<ServiceDetailPage />} />
+      </Routes>
+    </div>
   );
 }
 
